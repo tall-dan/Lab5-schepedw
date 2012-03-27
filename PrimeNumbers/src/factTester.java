e2c98f3cdcf3b65e86d817146e8fa950187098b4
Daniel Schepers <schepedw@rose-hulman.edu>
2012-03-26 20:43:53 -0400

    Third Test Case Written

--- PrimeNumbers/src/factTester.java
+++ PrimeNumbers/src/factTester.java
@@ -26,6 +26,12 @@ public class factTester {
 	}
 	
 	
+	@Test
+	public void testThree(){
+		assertEquals(list(3), PrimeNumbers.generatePrimes(3));
+	}
+	
+	
 	private ArrayList<Integer> list(int...ints){
 		ArrayList<Integer> ret =new ArrayList<Integer>();
 		for (int i : ints){

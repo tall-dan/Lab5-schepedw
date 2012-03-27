7ff7f93591a7ea2ba3bdc2e93c82363c62afa10b
Daniel Schepers <schepedw@rose-hulman.edu>
2012-03-26 20:28:41 -0400

    First Test Case Written

---
+++ PrimeNumbers/src/factTester.java
@@ -0,0 +1,23 @@
+
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author schepedw.
+ *         Created Mar 26, 2012.
+ */
+public class factTester {
+
+	
+	@Test
+	public void testOneIsEmpty(){
+		assertEquals(new ArrayList<Integer>(),PrimeNumbers.generatePrimes(1));
+	}
+	
+}

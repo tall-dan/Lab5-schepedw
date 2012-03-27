98a0bd61960a0b0440ccde33bbf8a3144c1aadab
Daniel Schepers <schepedw@rose-hulman.edu>
2012-03-26 20:50:27 -0400

    Fifth Test Case Written

--- PrimeNumbers/src/factTester.java
+++ PrimeNumbers/src/factTester.java
@@ -36,6 +36,11 @@ public class factTester {
 		assertEquals(list(2,2),PrimeNumbers.generatePrimes(4));
 	}
 	
+	@Test
+	public void testSix(){
+		assertEquals(list(2,3),PrimeNumbers.generatePrimes(6));
+	}
+	
 	private ArrayList<Integer> list(int...ints){
 		ArrayList<Integer> ret =new ArrayList<Integer>();
 		for (int i : ints){
